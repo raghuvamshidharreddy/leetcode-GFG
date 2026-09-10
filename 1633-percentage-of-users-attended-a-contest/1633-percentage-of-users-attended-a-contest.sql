33f94f82c65635eccc1472a81ff041168ca18466
Vudayagiri Raghuvamshidhar Reddy <raghuvamshidharreddy@gmail.com>
2026-09-10 22:54:05 +0530

    Time: 1001 ms (73.28%), Space: 0B (100%) - LeetHub

---
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select 
+    contest_id,
+    round(count(r.user_id)*100/(select count(user_id) from users),2) as percentage
+from register r
+group by contest_id
+order by percentage desc ,contest_id;
